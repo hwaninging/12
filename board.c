859884c17e1b54a8bee977bc3611a2ea30addd2f
hwaninging <hwanhee@sookmyung.ac.kr>
2025-11-20 14:50:19 +0900

    마무리 (다시)
    오류 있음

--- board.c
+++ board.c
@@ -1,14 +1,19 @@
-#if 0
-int board_getSHarkPosition(void);
+#if 0 //어떤게 있으면 좋을지 작성 (컴파일 안되게 하기) 
+int board_initBoard(void); //했다 
+int board_getBoardStatus(int pos); //했다 
+int board_getSharkPosition(void);
 int board_stepShark(void);
-#endif
+int board_getBoardCoin(int pos); //했다 
+void board_printBoardStatus(void); //했다 
+#endif 
 
-#define N_BOARD         20 //몇 칸 할지 정하는 거 
+#define N_BOARD          20 //몇 칸 할지 정하는 거 
 #define BOARDSTATUS_OK   1 //상어에 의해서 파손 or 안 파손. 
 #define BOARDSTATUS_NOK  0
-#define N_COINPOS       12
+#define N_COINPOS        12
 #defien MAX_COIN         4
 
+//전역변수 사용 
 static int board_status[N_BOARD];
 static int board_coin[N_BOARD];
 static int shark_position;
@@ -27,14 +32,11 @@ void board_printBoardStatus(void)
              printf("O");
       }
      printf("|\n");
-     printf("-------------------------------------------\n");
-     
-     printf("----------------BOARD COIN-----------------\n");
-     
-     
+     printf("---------------------------------------------");
 }
 
-void board_getBoardStatus(int pos)
+//보드 정보 제공 
+int board_getBoardStatus(int pos)
 {
      return board_status[pos];
 }
@@ -47,9 +49,10 @@ int board_getBoardCoin(int pos)
     return board_coin[pos];
 }
 
-int board_initBoard(void)
+void board_initBoard(void) //지금은 반환할게 없어서 void 
 {
     int i;
+    
     //initialize arrays
     for (i=0;i<N_BOARD;i++)
     {
@@ -63,9 +66,10 @@ int board_initBoard(void)
         do {
             int coinpos = rand()%N_BOARD;
             if (board_coin[coinpos] == 0)
-            {
-                board_coin[coinpos] = rand()%MAX_COIN + 1;
+            { //코인 할당하는 동작 
+                board_coin[coinpos] = rand()%MAX_COIN + 1; //COINMAX 안 넘기게 랜덤으로 코인  넣어줌. 
                 flag_allocated = 1;
             } 
-            } while(flag_allocated == 0); //코인 할당 했으면 여기 함수에서 나가도돼 
+        } while(flag_allocated == 0); //코인 할당 했으면 여기 함수에서 나가도돼 
+    }
 }
--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "board.h"
-#define 
+#include "board.h" 
+
+#define MAX_DIE   6
 //주사위 굴리는 함수 
 int rolldie(void)
 {
     return rand()%MAX_DIE + 1;
 }
 
-//게임 시작 및 종료를 알리는 출력 
 int main(int argc, char *argv[])
 {
-  int
-  int
+  int cnt; 
+  int pos;
   
   srand((unsigned) (time(NULL))); //한번 실행 해줘야됨. 
   //random한 값이 바로바로 나오도록 해줌.  
@@ -28,32 +28,33 @@ int main(int argc, char *argv[])
   
   //게임 구성 계획짜기 (요런식으로 할거다라는 의미) 
   //step 1. initialization (플레이어 이름 세팅, variavles)
-  int board_initBoard();
+  board_initBoard();
   
   //step 2. 본격적 게임 시작(do-while 반복문 사용)
-  cnt = 0;
+  cnt = 0; //0으로 초기화 
   pos = 0;
   int coinReslut = 0;
   do {
       int die_result;
+      int coinResult; 
       
-  // 2-1. status printing
-  board_printBoardStatus();
+    // 2-1. status printing
+    board_printBoardStatus();
   
-  // 2-2. roll die(규칙)
-  die_result = rolldie();
+    // 2-2. roll die(규칙)
+    die_result = rolldie();
   
-  // 2-3. move(result)
-  pos += die_result;
+    // 2-3. move(result)
+    pos += die_result; //원래 위치에다가 die_result를 더해줌. 
   
-  printf("pos : %i (die:roll) \n"); //코인을 이만큼 얻었다.
+    printf("pos : %i (die:%i)\n", pos, die_result); //코인을 이만큼 얻었다.
   
-  coinResult = board_getBoardCoin(pos);
+    coinResult = board_getBoardCoin(pos); //요기 위치에 있는 코인 가져옴. 
   
-  printf("coin : %i\n", coinResult); //코인을 이만큼 얻었다. 
-  // 2-4. change turn, shark move 
-  cnt ++;
-} while(cnt < 5);
+    printf("coin : %i\n", coinResult); //코인을 이만큼 얻었다. 
+    // 2-4. change turn, shark move 
+    cnt++;
+    } while(cnt < 5);
   
   //step 3. game end(이긴 사람 알려주기) - 반복문에서 벗어나면 끝 
   
